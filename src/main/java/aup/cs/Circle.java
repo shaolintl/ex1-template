f0310ecf15660cbe6b65015e9c6e0075c74662d2
Tomer <shaolintl@gmail.com>
2021-01-28 09:24:34 +0000

    Finished class 6

--- src/main/java/aup/cs/Circle.java
+++ src/main/java/aup/cs/Circle.java
@@ -4,14 +4,22 @@ public class Circle {
     double radius;
 
     public static void main(String[] args) {
-        Circle c1 = new Circle();
-        c1.radius = 4.5;
+        Circle c1 = new Circle(4.5);
         Circle c2 = new Circle();
-        c2.radius = 40.3;
+        c2.radius = 31;
         System.out.println("Circle 1 has an area of " + c1.computeArea());
         System.out.println("Circle 2 has an area of " + c2.computeArea());
+
+        Circle.radius;
     }
 
+    Circle(double rad) {
+        radius = rad;
+    }
+
+    Circle() {
+        radius = 1.0;
+    }
     double computeArea() {
         return Math.PI * Math.pow(radius,2);
     }
