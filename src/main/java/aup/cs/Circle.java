b720d4c82f40c65f15496f38a44a962c375909a3
Tomer <shaolintl@gmail.com>
2021-02-03 14:06:33 +0000

    class 8

--- src/main/java/aup/cs/Circle.java
+++ src/main/java/aup/cs/Circle.java
@@ -1,19 +1,55 @@
 package aup.cs;
 
 class Circle {
-    private double axe;
 
+    private double radius;
+    private static int numberOfObjects = 0;
+    
     public static void main(String[] args) {
-        Circle c1 = new Circle();
-        Circle c2 = new Circle();
-        System.out.println(c1.getRadius());
+        Circle[] circles = getCircles();
+        String value = displayCirclesArea(circles);
+        System.out.println("The total area is " + value);
+        System.out.println("The number of circles is " + Circle.getNumberOfObjects());
+    }
+
+    private static Circle[] getCircles() {
+        return new Circle[] {
+            new Circle(5.0),
+            new Circle(2.0),
+            new Circle(-3.0),
+            new Circle(121.0),
+            new Circle(122.34)
+        };
+    }
+
+    private static String displayCirclesArea(Circle[] circles) {
+        double acc = 0.0;
+        int i;
+        for (i=0; i<circles.length; i++) {
+            acc += circles[i].getArea();
+        }
+        return "" + acc + " of " + i + " circles";
+    }
+
+    public Circle() {
+        this(0.0);
+    }
+
+    public Circle(double radius) {
+        numberOfObjects++;
+        this.radius = radius;
     }
 
     public double getRadius() {
-        return axe;
+        // check permissions
+        return radius;
     }
 
-    public void setRadius(double r) {
-        axe = r;
+    public static int getNumberOfObjects() {
+        return numberOfObjects;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius,2);
     }
 }
